d7eccebc8897f4fcaaacd1922c0342b061693b8b
Miyuki9 <felixmaundu1@gmail.com>
2023-08-02 11:09:00 -0700

    Check existance of char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ *
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a=0;
+	while(s[a] != '\0')
+	{
+		if(s[a] == c)
+		{
+			return (s+a);
+		}
+		a++;
+	}
+	if(s[a] == '\0')
+	{
+		return (NULL);
+	}
+}
