460de746c2710398d7545cf7ee717089e6df06f9
Miyuki9 <felixmaundu1@gmail.com>
2023-08-09 07:23:35 -0700

    Second correction on concat array

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 		len2++;
 	total = len1 + len2;
 
-	s3 = malloc(sizeof(char) * total);
+	s3 = malloc(sizeof(char) * (total + 1));
 	if (s3 == NULL)
 		return (NULL);
 
